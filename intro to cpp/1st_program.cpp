571eb3b990ce8741975132c8129130a24efb8a2a
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-23 13:07:11 +0530

    Auto update 2026-07-23 13:07:11

--- intro to cpp/1st_program.cpp
+++ intro to cpp/1st_program.cpp
@@ -4,6 +4,6 @@ using namespace std; // standard namespace
 
 int main() {
 
-    cout << "My Name : Nischal.k" << endl;
+    cout << "My Name : Nischal.k" << endl; //cout - keyword 
     return 0;
 }
