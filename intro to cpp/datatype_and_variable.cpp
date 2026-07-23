0c0e27aa141a9d2c130a716111564ba0f502742e
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-23 18:43:56 +0530

    Auto update 2026-07-23 18:43:56

--- intro to cpp/datatype_and_variable.cpp
+++ intro to cpp/datatype_and_variable.cpp
@@ -16,28 +16,28 @@ int main () {
     // age = 101;
     // cout << age << endl ; 
 
-    //int
-    int count = 5;
-    //float 
-    int share = 3.14;
-    //char
-    char alphabet = 'z';
-    //double
-    double weight = 55.9999999;
-    //boolean
-    bool isMale = true;
-    bool isChild = 1;
-    bool isFemale = false;
-    bool isGril = 0;
+    // //int
+    // int count = 5;
+    // //float 
+    // int share = 3.14;
+    // //char
+    // char alphabet = 'z';
+    // //double
+    // double weight = 55.9999999;
+    // //boolean
+    // bool isMale = true;
+    // bool isChild = 1;
+    // bool isFemale = false;
+    // bool isGril = 0;
 
-    cout << count << endl ;
-    cout << share << endl ;
-    cout << alphabet << endl ;
-    cout << weight << endl ;
-    cout << isMale << endl ;
-    cout << isChild << endl ;
-    cout << isFemale << endl ;
-    cout << isGril << endl ;
+    // cout << count << endl ;
+    // cout << share << endl ;
+    // cout << alphabet << endl ;
+    // cout << weight << endl ;
+    // cout << isMale << endl ;
+    // cout << isChild << endl ;
+    // cout << isFemale << endl ;
+    // cout << isGril << endl ;
 
 
     return 0;
