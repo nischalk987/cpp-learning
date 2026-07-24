11bc6640834f6b331ccb0201049e1bd8ee994122
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-24 11:30:19 +0530

    Auto update 2026-07-24 11:30:19

--- intro to cpp/switch_case.cpp
+++ intro to cpp/switch_case.cpp
@@ -11,8 +11,8 @@ int main() {
     {
     case 'A':cout << "90 to 100 " << endl;
     case 'B':cout << "80 to 90 " << endl;
-    case 'C':cout << " to 100 " << endl;
-    case 'D':cout << "90 to 100 " << endl;
+    case 'C':cout << "70 to 80 " << endl;
+    case 'D':cout << "60 to 100 " << endl;
     default:
         break;
     }
