a71c0e18cc88bed42a792024c9e5399ceeb1061e
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-27 10:38:31 +0530

    Auto update 2026-07-27 10:38:31

--- intro to cpp/ternary_operator.cpp
+++ intro to cpp/ternary_operator.cpp
@@ -3,9 +3,11 @@ using namespace std ;
 
 int main(){
 
-    int age ;
-    cout << "Enter your age : " << endl ;
-    cin >> age ;
+//     int age ;
+//     cout << "Enter your age : " << endl ;
+//     cin >> age ;
 
-    (age >= 18) ? cout << "You have votting rights" : cout << "You don't have votting rights" ;
-}
+//     (age >= 18) ? cout << "You have votting rights" : cout << "You don't have votting rights" ;
+// 
+
+
