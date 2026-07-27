782d117ef3d31d31d38c816b3f4b5569b6f21d9d
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-27 10:53:45 +0530

    Auto update 2026-07-27 10:53:45

--- intro to cpp/loop_statement.cpp
+++ intro to cpp/loop_statement.cpp
@@ -4,7 +4,7 @@ using namespace std ;
 int main() {
 
     for (int i = 1; i>=10 ; i=i+1){
-        cout << "Hello word " << endl; 
+        cout << "Hello word " << i <<  endl; 
     }
 
     return 0 ;
