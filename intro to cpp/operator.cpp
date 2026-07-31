f002ba709daa3d68f72d67607d68880a2b421fe5
Nischal K <YOUR_EMAIL@gmail.com>
2026-07-31 11:18:20 +0530

    Auto update 2026-07-31 11:18:20

--- intro to cpp/operator.cpp
+++ intro to cpp/operator.cpp
@@ -5,16 +5,16 @@ int main () {
 
     // Arithmatic operator 
 
-    int a;
-    int b;
-    cout << "Enter first number : " << " " ;
-    cin >> a;
-    cout << "Enter second number : " << " ";
-    cin >> b ;
-    cout << "Addition = " << a + b << endl ;
-    cout << "subtraction = " << a - b << endl ;
-    cout << "multiplication = " << a * b << endl ;
-    cout << "division  = " << a / b << endl;
-    cout << "modulas = " << a % b << endl ;
+    // int a;
+    // int b;
+    // cout << "Enter first number : " << " " ;
+    // cin >> a;
+    // cout << "Enter second number : " << " ";
+    // cin >> b ;
+    // cout << "Addition = " << a + b << endl ;
+    // cout << "subtraction = " << a - b << endl ;
+    // cout << "multiplication = " << a * b << endl ;
+    // cout << "division  = " << a / b << endl;
+    // cout << "modulas = " << a % b << endl ;
 
 }
