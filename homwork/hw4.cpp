3cbc677de12dea04f3bb578f360ae98cd44eb384
Nischal K <nischalk789@gmail.com>
2026-08-08 12:10:59 +0530

    Auto update 2026-08-08 12:10:59

--- homwork/hw4.cpp
+++ homwork/hw4.cpp
@@ -66,18 +66,20 @@ int main () {
     //     }
     // }
 
-    bool isPrime = true ;
-    for (int i=2 ; i<= n-1 ; i++){
-        if(n%i == 0) {
-            isPrime = false ;
-            break ;
-        }
-    }
-    if (isPrime == true) {
-        cout << "The number is prime." << endl ;
-    }
-    else {
-        cout << "The number is not prime." << endl ;
-    }
-    return 0 ;
+    // bool isPrime = true ;
+    // for (int i=2 ; i<= n-1 ; i++){
+    //     if(n%i == 0) {
+    //         isPrime = false ;
+    //         break ;
+    //     }
+    // }
+    // if (isPrime == true) {
+    //     cout << "The number is prime." << endl ;
+    // }
+    // else {
+    //     cout << "The number is not prime." << endl ;
+    // }
+    // return 0 ;
+
+    
 }
