db160d3ec4507b627f03d129ce046152864710d1
Nischal K <nischalk789@gmail.com>
2026-08-10 15:42:18 +0530

    Auto update 2026-08-10 15:42:17

--- intro to cpp/patterns.cpp
+++ intro to cpp/patterns.cpp
@@ -86,7 +86,7 @@ int main () {
     int n = 4 ;
     int num = 1 ;
     for(int i=0 ; i <=n ; i++){
-        for(int j=0 ; j<=i+1 ; j++) {
+        for(int j=0 ; j<=i ; j++) {
             cout << num << " " ;
             num ++ ;
         }
