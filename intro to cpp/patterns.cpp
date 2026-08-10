1958db98213e0f4eeb8600ec85387fd7846baa9e
Nischal K <nischalk789@gmail.com>
2026-08-10 11:19:33 +0530

    Auto update 2026-08-10 11:19:33

--- intro to cpp/patterns.cpp
+++ intro to cpp/patterns.cpp
@@ -24,7 +24,7 @@ int main () {
     // }
 
     char n = 6 ;
-    for (int i =1 ; i <=n ; i++) {
+    for (char i = "a" ; i <=n ; i++) {
         for (int j = 1 ; j<=n ; j++) {
             cout << "*" << " " ;
         }
