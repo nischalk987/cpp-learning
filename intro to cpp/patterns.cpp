f2bc92387a0d9159c34b46214e3950f5c9db85cb
Nischal K <nischalk789@gmail.com>
2026-08-10 11:14:42 +0530

    Auto update 2026-08-10 11:14:42

--- intro to cpp/patterns.cpp
+++ intro to cpp/patterns.cpp
@@ -5,11 +5,11 @@ int main () {
 
     // Square Patterns 
 
-    int n = 4 ;
-    for(int i = 1 ; i <=n ; i++){
-        for (int j =1 ; j<=n ; j++) {
-            cout << j << " " ;
-        }
-        cout << endl ; 
-    }
+    // int n = 4 ;
+    // for(int i = 1 ; i <=n ; i++){
+    //     for (int j =1 ; j<=n ; j++) {
+    //         cout << j << " " ;
+    //     }
+    //     cout << endl ; 
+    // }
 }
