ac5f309c734b66495b9c052992dccf4d223a5187
Nischal K <nischalk789@gmail.com>
2026-08-10 12:32:09 +0530

    Auto update 2026-08-10 12:32:09

--- intro to cpp/patterns.cpp
+++ intro to cpp/patterns.cpp
@@ -57,7 +57,7 @@ int main () {
     // }
 
     // Trianglr pattern 
-    int n = 3 ;
+    int n = 7 ;
     for (int i = 0 ; i < n ; i++) {
         for (int j = 0 ; j < i+1 ; j++) {
             cout << " * " << " " ;
