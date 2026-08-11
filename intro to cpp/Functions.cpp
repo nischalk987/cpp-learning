8bb785cc96fb7a13e23cd48740283c7af20aebbf
Nischal K <nischalk789@gmail.com>
2026-08-11 11:08:48 +0530

    Auto update 2026-08-11 11:08:48

--- intro to cpp/Functions.cpp
+++ intro to cpp/Functions.cpp
@@ -1,13 +1,15 @@
 #include<iostream>
 using namespace std ; 
 
-int sumofnum(int n) {
-    int sum = 0 ; 
-    for(int i = 1 ; i <= n ; i++) {
-        sum += i ; 
-    }
-    return sum ;
-}
+// int sumofnum(int n) {
+//     int sum = 0 ; 
+//     for(int i = 1 ; i <= n ; i++) {
+//         sum += i ; 
+//     }
+//     return sum ;
+// }
+
+
 
 int main () {
     
