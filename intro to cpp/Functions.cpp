880786a5ff2ed5341626b7b7763716f92ae4f635
Nischal K <nischalk789@gmail.com>
2026-08-11 11:03:33 +0530

    Auto update 2026-08-11 11:03:33

--- intro to cpp/Functions.cpp
+++ intro to cpp/Functions.cpp
@@ -2,3 +2,9 @@
 using namespace std ; 
 
 void sumofnum(int n) {
+    int sum = 0 ; 
+    for(int i = 1 ; i <= n ; i++) {
+        sum += i ; 
+    }
+    cout << "The sum of first " << n << " natural numbers is : " << sum << endl ; 
+}
