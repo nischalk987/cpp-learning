afbd4c9426e265368a54a944e3c1efd965a01576
Nischal K <nischalk789@gmail.com>
2026-08-11 12:00:19 +0530

    Auto update 2026-08-11 12:00:19

--- intro to cpp/Functions.cpp
+++ intro to cpp/Functions.cpp
@@ -13,7 +13,7 @@ int factorial (int n , int r ) {
     int fact = 1 ;
     for (int i = 1 ; i <= n ; i++) {
         fact = fact * i ;
-        int bimomial = (n/r)
+        int bimomial = (n/r(n-r)) ;
     }
     return fact ;
 }
