73188d994a55e524eb55188a22cff6dd096eac6c
Nischal K <nischalk789@gmail.com>
2026-08-11 12:10:21 +0530

    Auto update 2026-08-11 12:10:21

--- intro to cpp/Functions.cpp
+++ intro to cpp/Functions.cpp
@@ -59,7 +59,7 @@ int main () {
     cout << "Enter a r number : " << " ";
     cin >> r ;
 
-    cout << "Binomial cofficient : " << binomial(n)
+    cout << "Binomial cofficient : " << 
 
     return 0 ;
 }
